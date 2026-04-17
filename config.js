9289190a90f26f3309326352d2c4079b4bd36661
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-17 16:05:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 🔥 SINGLE SOURCE API CONFIG
-const API_BASE = "https://script.google.com/macros/s/AKfycbwkdafYPoII7QgrZmDMVutFIDFkoyZhQdV3PJS_24RweetdeBaaNa7ZDzN6AtGSmCQC4A/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbwOaMgCMYMmR-WO7Cx38PjPYKjp9oBYKtSSS63d9aL0R5S6f5aqNu2IUG0sFdavlt5FHQ/exec";
 
 // 🔥 ALL ENDPOINTS
 const API = {
